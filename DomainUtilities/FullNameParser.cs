a88b4376b9a56061c1bf6a1d8208ae4fc03b3fc9
Todd Dill <todd@tetranetic.com>
2014-05-20 09:49:10 -0500

    Added full name parser and associated test harness

---
+++ DomainUtilities/FullNameParser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DomainUtilities
+{
+    public class FullNameParser
+    {
+        public static Name Parse(string fullName)
+        {
+            Name name = new Name();
+            string fullNamePattern = "^(?<title>.*\\.\\s)*(?<firstname>([A-Z][a-z]+\\s*)+)(\\s)(?<middleinitial>([A-Z]\\.?\\s)*)(?<lastname>[A-Z][a-zA-Z-']+)(?<suffix>.*)$";
+
+            Regex regex = new Regex(fullNamePattern);
+            MatchCollection matches = regex.Matches(fullName);
+
+            if (matches.Count == 0) throw new ArgumentException("Cannot parse name supplied.  Provide a valid space separated full name.", "fullname");
+
+            foreach (Match match in matches)
+            {
+                name.Title = match.Groups["title"].Value.Trim();
+                name.First = match.Groups["firstname"].Value.Trim();
+                name.Middle = match.Groups["middleinitial"].Value.Trim();
+                name.Last = match.Groups["lastname"].Value.Trim();
+                name.Suffix = match.Groups["suffix"].Value.Trim();
+            }
+            
+            return name;
+        }
+    }
+
+    public class Name
+    {
+        public string Title { get; set; }
+        public string First { get; set; }
+        public string Middle { get; set; }
+        public string Last { get; set; }
+        public string Suffix { get; set; }
+
+        public Name()
+        {
+            Title = string.Empty;
+            First = string.Empty;
+            Middle = string.Empty;
+            Last = string.Empty;
+            Suffix = string.Empty;
+        }
+
+        public string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var property in this.GetType().GetProperties())
+            {
+                string value = (string)property.GetValue(this, null);
+                if (!string.IsNullOrEmpty(value))
+                {
+                    sb.Append(value);
+                    sb.Append(" ");
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ DomainUtilitiesTests/FullNameParserTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainUtilities;
+
+namespace DomainUtilitiesTests
+{
+    /// <summary>
+    /// Summary description for FullNameParserTests
+    /// </summary>
+    [TestClass]
+    public class FullNameParserTests
+    {
+        public FullNameParserTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+         	
+        //Mary White, Mary Jo White, Dr. Mary Jo White, Dr. Mary Jo T. White, Ph.D.
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FullNameParserOnlyFirst()
+        {
+            Name expected = CreateNameObject("first");
+            Name actual = FullNameParser.Parse(expected.ToString());
+
+            Assert.AreEqual(actual.First, expected.First);
+            Assert.AreEqual(actual.Last, expected.Last);
+            Assert.AreEqual(actual.Title, expected.Title);
+            Assert.AreEqual(actual.Suffix, expected.Suffix);
+            Assert.AreEqual(actual.Middle, expected.Middle);
+        }
+
+        [TestMethod]
+        public void FullNameParserFirstAndLast()
+        {
+            Name expected = CreateNameObject("first", "last");
+            Name actual = FullNameParser.Parse(expected.ToString());
+
+            Assert.AreEqual(actual.First, expected.First);
+            Assert.AreEqual(actual.Last, expected.Last);
+            Assert.AreEqual(actual.Title, expected.Title);
+            Assert.AreEqual(actual.Suffix, expected.Suffix);
+            Assert.AreEqual(actual.Middle, expected.Middle);
+        }
+
+        [TestMethod]
+        public void FullNameParserFirstMiddleLast()
+        {
+            Name expected = CreateNameObject("first", "last", "middle");
+            Name actual = FullNameParser.Parse(expected.ToString());
+
+            Assert.AreEqual(actual.First, expected.First);
+            Assert.AreEqual(actual.Last, expected.Last);
+            Assert.AreEqual(actual.Title, expected.Title);
+            Assert.AreEqual(actual.Suffix, expected.Suffix);
+            Assert.AreEqual(actual.Middle, expected.Middle);
+        }
+
+        [TestMethod]
+        public void FullNameParserTitleFirstLast()
+        {
+            Name expected = CreateNameObject("first", "last", "title");
+            Name actual = FullNameParser.Parse(expected.ToString());
+
+            Assert.AreEqual(actual.First, expected.First);
+            Assert.AreEqual(actual.Last, expected.Last);
+            Assert.AreEqual(actual.Title, expected.Title);
+            Assert.AreEqual(actual.Suffix, expected.Suffix);
+            Assert.AreEqual(actual.Middle, expected.Middle);
+        }
+
+        public Name CreateNameObject(params string[] elements)
+        {
+            Name name = new Name();
+
+            foreach (string element in elements)
+            {
+                if (element.Equals("title")) name.Title = "Dr.";
+                if (element.Equals("first")) name.First = "Peter";
+                if (element.Equals("middle")) name.Middle = "A.";
+                if (element.Equals("last")) name.Last = "Gibbons";
+                if (element.Equals("suffix")) name.Suffix = "Jr.";
+            }
+
+            return name;
+        }
+    }
+}
