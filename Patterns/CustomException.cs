d8be8c4ecf029b6af0e59aeffc4ec86adea4cc4f
Todd Dill <todd@tetranetic.com>
2014-05-25 07:18:56 -0500

    Added pattern examples

---
+++ Patterns/CustomException.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Patterns
+{
+    /// <summary>
+    /// Sample exception
+    /// </summary>
+    /// <remarks>
+    /// tdill - 4/22/2008 9:59 AM
+    /// </remarks>
+    [Serializable()]
+    public class MyException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyException"/> class.
+        /// </summary>
+        /// <remarks>
+        /// tdill - 4/22/2008 10:00 AM
+        /// </remarks>
+        public MyException() : base() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <remarks>
+        /// tdill - 4/22/2008 10:00 AM
+        /// </remarks>
+        public MyException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
+        /// <remarks>
+        /// tdill - 4/22/2008 10:00 AM
+        /// </remarks>
+        public MyException(string message, Exception inner) : base(message, inner) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyException"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is null. </exception>
+        /// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0). </exception>
+        /// <remarks>
+        /// tdill - 4/22/2008 9:59 AM
+        /// </remarks>
+        protected MyException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
+
---
+++ Patterns/Singleton.cs
@@ -0,0 +1,36 @@
+﻿namespace Patterns
+{
+    /// <summary>
+    /// Lazy Instantiation
+    /// </summary>
+    public sealed class Singleton
+    {
+        Singleton()
+        {
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static Singleton Instance
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        class Nested
+        {
+            // Explicit static constructor to tell C# compiler
+            // not to mark type as beforefieldinit
+            static Nested()
+            {
+            }
+
+            internal static readonly Singleton instance = new Singleton();
+        }
+    }
+}
+
